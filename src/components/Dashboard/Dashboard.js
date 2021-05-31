cd6f18ba2ffaa12d4304282ddb882b42dfc3a8da
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-05-31 09:47:45 +0600

    loading spinner added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Table } from 'react-bootstrap';
+import { Pagination, Table } from 'react-bootstrap';
 import Filter from '../Filter/Filter';
 import LaunchList from '../LaunchList/LaunchList';
+import spinner from '../../images/spinner.gif';
 import './Dashboard.css'
 
 const Dashboard = () => {
     const [launches, setLaunches] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch('https://api.spacexdata.com/v3/launches')
             .then(res => res.json())
-            .then(data => setLaunches(data.slice(0, 12)))
+            .then(data => {
+                setLaunches(data.slice(0, 12));
+                setLoading(false);
+            })
     }, [])
 
     return (
@@ -30,12 +35,36 @@ const Dashboard = () => {
                             <th>Rocket</th>
                         </tr>
                     </thead>
+
                     <tbody className="table-row">
                         {
                             launches.map(launch => <LaunchList launch={launch} key={launch.flight_number}></LaunchList>)
                         }
                     </tbody>
                 </Table>
+                {
+                    loading &&
+                    <div className="d-flex justify-content-center align-items-center">
+                        <img src={spinner} alt="spinner" className="spinner" />
+                    </div>
+                }
+            </div>
+
+            <div className="pagination-container d-flex justify-content-end mb-4">
+                <Pagination>
+                    <Pagination.Prev />
+                    <Pagination.Item >{1}</Pagination.Item>
+                    <Pagination.Item>{2}</Pagination.Item>
+                    <Pagination.Item>{3}</Pagination.Item>
+                    <Pagination.Item >{4}</Pagination.Item>
+                    <Pagination.Ellipsis />
+
+                    <Pagination.Item>{13}</Pagination.Item>
+
+                    <Pagination.Ellipsis />
+                    <Pagination.Item>{20}</Pagination.Item>
+                    <Pagination.Next />
+                </Pagination>
             </div>
 
         </section>
--- src/components/ModalCalendar/ModalCalendar.js
+++ src/components/ModalCalendar/ModalCalendar.js
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
 import 'react-calendar/dist/Calendar.css';
 import Calendar from 'react-calendar';
+import { Link } from 'react-router-dom';
 
 const ModalCalendar = () => {
     const [value, onChange] = useState(new Date());
 
+    const HandlePast = () => {
+        alert("opps, sorry ! It is under developing. Please try latter")
+    }
+
     return (
         <div className="row">
             <div className="col">
-                <h6>Past week</h6>
-                <h6>Past month</h6>
-                <h6>Past 3 month</h6>
-                <h6>Past 6 month</h6>
-                <h6>Past year</h6>
-                <h6>Past 2 years</h6>
+                <Link onClick={HandlePast} className="text-decoration-none"><h6>Past week</h6></Link>
+                <Link onClick={HandlePast} className="text-decoration-none"><h6>Past month</h6></Link>
+                <Link onClick={HandlePast} className="text-decoration-none"><h6>Past 3 month</h6></Link>
+                <Link onClick={HandlePast} className="text-decoration-none"><h6>Past 6 month</h6></Link>
+                <Link onClick={HandlePast} className="text-decoration-none"><h6>Past year</h6></Link>
+                <Link onClick={HandlePast} className="text-decoration-none"><h6>Past 2 years</h6></Link>
             </div>
             <div className="col-8">
                 <Calendar
